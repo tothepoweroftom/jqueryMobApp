343ab5a9f25bf30dde13c0a9af812fd0cc108e0a
Tom Power <tom.power@mobgen.com>
2017-01-11 10:11:11 +0100

    [UPDATE]
    Add communcation to console code.

--- app.js
+++ app.js
@@ -15,3 +15,46 @@ nx.onload = function() {
     console.log("data-x: " + data.x + " data-y: " + data.y);
   })
 }
+
+$(function() {
+
+
+    $(".dial").knob({
+      'height': 350,
+      'width' : 350,
+      'change' : function (v) { console.log(v) }
+
+
+    });
+
+});
+
+function switchFlipped(){
+  console.log($('#flip').val());
+
+}
+
+function refreshPressed(){
+
+  console.log("Refreshed");
+}
+
+function slider1Changed(){
+  console.log($('.tweetSlider').val());
+
+}
+
+function slider2Changed(){
+  console.log($('#slider-2').val());
+
+}
+
+function slider3Changed(){
+  console.log($('#slider-3').val());
+
+}
+
+function slider4Changed(){
+  console.log($('#slider-4').val());
+
+}
